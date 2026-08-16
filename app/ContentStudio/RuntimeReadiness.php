c5333d5139dc45251bd214a7108560b6cb1cdc7d
jeffryroest <jeffry.roest@gmail.com>
2026-08-16 13:35:16 +0200

    fix: wrap promoted constructor parameter

--- app/ContentStudio/RuntimeReadiness.php
+++ app/ContentStudio/RuntimeReadiness.php
@@ -8,7 +8,9 @@ use App\Models\ContentNode;
 
 final class RuntimeReadiness
 {
-    public function __construct(private readonly PublishedContentRepository $publishedContent) {}
+    public function __construct(
+        private readonly PublishedContentRepository $publishedContent,
+    ) {}
 
     /** @return list<array<string, mixed>> */
     public function items(): array
