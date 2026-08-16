064e307000c9cebb32e60a0d585264b43c76c43e
jeffryroest <jeffry.roest@gmail.com>
2026-08-16 13:36:38 +0200

    fix: use explicit readiness dependency

--- app/ContentStudio/RuntimeReadiness.php
+++ app/ContentStudio/RuntimeReadiness.php
@@ -8,9 +8,12 @@ use App\Models\ContentNode;
 
 final class RuntimeReadiness
 {
-    public function __construct(
-        private readonly PublishedContentRepository $publishedContent,
-    ) {}
+    private readonly PublishedContentRepository $publishedContent;
+
+    public function __construct(PublishedContentRepository $publishedContent)
+    {
+        $this->publishedContent = $publishedContent;
+    }
 
     /** @return list<array<string, mixed>> */
     public function items(): array
